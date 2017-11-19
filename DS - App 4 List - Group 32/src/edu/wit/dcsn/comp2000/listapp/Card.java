d2820a6222abdd64077433e1aa745e163da9221c
PapaPajama <PapaPajama@192.168.86.201>
2017-11-19 14:15:43 -0500

    Javadocs and Tests for Card and Player

--- DS - App 4 List - Group 32/src/edu/wit/dcsn/comp2000/listapp/Card.java
+++ DS - App 4 List - Group 32/src/edu/wit/dcsn/comp2000/listapp/Card.java
@@ -1,7 +1,8 @@
 package edu.wit.dcsn.comp2000.listapp;
 /**
- * 
- * @author PapaPajama
+ * This class represents the cards in a deck. It gives a card an enum Type, and an enum Value. 
+ * There are A card can have one of four types as well as one value.
+ * @author Alec Carbonell
  *
  */
 public class Card {
@@ -25,30 +26,62 @@ public class Card {
 	private Value value;
 	private boolean isFaceUp;
 	
+	/** constructs the object Card
+	 * @param type
+	 * @param value
+	 */
 	public Card(Type type, Value value) {
 		this.type = type;
 		this.value = value;
 	}
 	
+	/** gets the type of the card object
+	 * 
+	 * @return 
+	 */
 	public Type getType() {
 		return type;
 	}
-	
+	/** gets the value of the card object
+	 * 
+	 * @return 
+	 */
 	public Value getValue() {
 		return value;
 	}
 	
+	/** changes the boolean isFaceUp to the opposite of what it currently is.
+	 * 
+	 */
 	public void flip() {
 		isFaceUp = !isFaceUp;
 		// TODO: Implement 
 	}
 	
+	/** determines whether or not the card in question is facing up
+	 * 
+	 * @return
+	 */
 	public boolean isFaceUp() {
 		return isFaceUp;
 	}
 	
+	/** A toString method that will return the value and type of the card as a string.
+	 * 
+	 */
 	public String toString() { 
 		return "This is the " + value + " of " + type;
 		//displays the the Type and value of card
+	}
+//------------------------------------------------ Tests ----------------------------------------------------------- \\
+
+	public static void main(String[]args) {
+		Card card = new Card(Type.Hearts, Value._2 );
+		System.out.println(card.getType());
+		System.out.println(card.getValue());
+		System.out.println(card.isFaceUp());
+		card.flip();
+		System.out.println(card.isFaceUp());
+		System.out.println(card.toString());
 	}
 }
--- DS - App 4 List - Group 32/src/edu/wit/dcsn/comp2000/listapp/Player.java
+++ DS - App 4 List - Group 32/src/edu/wit/dcsn/comp2000/listapp/Player.java
@@ -1,7 +1,8 @@
 package edu.wit.dcsn.comp2000.listapp;
 /** 
- * 
- * @author PapaPajama
+ * This class represents a player in the game. The player can draw cards from the deck as well as see their cards
+ * and count the total. If the total in hand is over 21, the player is unable to draw from the deck again.
+ * @author Alec Carbonell
  *
  */
 public class Player extends Deck{
@@ -10,12 +11,22 @@ public class Player extends Deck{
 	
 	private final int ID;
 	
+	/** constructs the player object
+	 * 
+	 * @param id
+	 * @param name
+	 */
 	public Player(int id, String name) {
 		this.ID = id;
 		this.name = name;
 		hand = new Hand();
 	}
 	
+	/** allows the player to draw from the deck to their hand. If the player's hand count totals more than 21.
+	 * the player is unable to draw again.
+	 * @param deck
+	 * @return
+	 */
 	public boolean hit(Deck deck) {
 		hand.hit(deck);
 		if (hand.sum()>21)
@@ -24,19 +35,43 @@ public class Player extends Deck{
 			return true;
 	}
 	
+	/** Gets the name of the player
+	 * 
+	 * @return
+	 */
 	public String getName() {
 		return name;
 	}
 	
+	/** gets the ID of the player.
+	 * 
+	 * @return
+	 */
 	public int getID() {
 		return ID;
 	}
 	
+	/** gets the cards in a player's hand.
+	 * 
+	 * @return
+	 */
 	public Hand getHand() {
 		return hand;
 	}
 	
+	/** returns the player's, hand, and sum in hand as a string.
+	 * 
+	 */
 	public String toString() {
 		return "The Player " + name + " has a hand of " + hand + " totalling " + hand.sum();
+	}
+//	------------------------------------------------ Tests ----------------------------------------------------------- \\
+	
+	public static void main(String[]args) {
+		Player player1 = new Player (1, "Alec");
+		System.out.println(player1.getName());
+		System.out.println(player1.getID());
+		System.out.println(player1.getHand());
+		System.out.println(player1.toString());
 	}
 }
